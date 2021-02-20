e5b3e6ba78e301124cad3a201a803113552e22cd
Chris Wein <chriswein12@yahoo.com>
2021-02-20 01:40:19 -0600

    Added Stripe payment

--- client/src/components/Cart/index.js
+++ client/src/components/Cart/index.js
@@ -5,22 +5,38 @@ import './style.css';
 import { useStoreContext } from '../../utils/GlobalState';
 import { TOGGLE_CART, ADD_MULTIPLE_TO_CART } from "../../utils/actions";
 import { idbPromise } from "../../utils/helpers";
+import { QUERY_CHECKOUT } from '../../utils/queries';
+import { loadStripe } from '@stripe/stripe-js';
+import { useLazyQuery } from '@apollo/react-hooks';
+
+
+const stripePromise = loadStripe('pk_test_TYooMQauvdEDq54NiTphI7jx');
 
 
 const Cart = () => {
 
     const [state, dispatch] = useStoreContext();
+    const [getCheckout, { data }] = useLazyQuery(QUERY_CHECKOUT);
+
 
     useEffect(() => {
         async function getCart() {
-          const cart = await idbPromise('cart', 'get');
-          dispatch({ type: ADD_MULTIPLE_TO_CART, products: [...cart] });
+            const cart = await idbPromise('cart', 'get');
+            dispatch({ type: ADD_MULTIPLE_TO_CART, products: [...cart] });
         };
-      
+
         if (!state.cart.length) {
-          getCart();
+            getCart();
         }
-      }, [state.cart.length, dispatch]);
+    }, [state.cart.length, dispatch]);
+
+    useEffect(() => {
+        if (data) {
+          stripePromise.then((res) => {
+            res.redirectToCheckout({ sessionId: data.checkout.session });
+          });
+        }
+      }, [data]);
 
     function toggleCart() {
         dispatch({ type: TOGGLE_CART });
@@ -35,6 +51,20 @@ const Cart = () => {
     }
 
     console.log(state)
+
+    function submitCheckout() {
+        const productIds = [];
+      
+        state.cart.forEach((item) => {
+          for (let i = 0; i < item.purchaseQuantity; i++) {
+            productIds.push(item._id);
+          }
+        });
+
+        getCheckout({
+            variables: { products: productIds }
+          });
+      }
 
     if (!state.cartOpen) {
         return (
@@ -59,7 +89,7 @@ const Cart = () => {
                         <strong>Total: ${calculateTotal()}</strong>
                         {
                             Auth.loggedIn() ?
-                                <button>
+                                <button onClick={submitCheckout}>
                                     Checkout
                                 </button>
                                 :
@@ -68,12 +98,12 @@ const Cart = () => {
                     </div>
                 </div>
             ) : (
-                <h3>
-                    <span role="img" aria-label="shocked">
-                        😱
+                    <h3>
+                        <span role="img" aria-label="shocked">
+                            😱
                     </span>
                     You haven't added anything to your cart yet!
-                </h3>
+                    </h3>
                 )}
         </div>
     );
